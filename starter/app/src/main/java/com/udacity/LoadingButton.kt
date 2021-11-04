3add0c511025deab8ec0f7c3a6adb3e349642831
Arlene Gustafson <arlene@gustafsonfamily.org>
2021-11-03 22:32:37 -0600

    Added GUI code along with code for custom button and code to download zipped files from 3 github repo choices.

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,9 +2,10 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -12,22 +13,102 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+    private var pointPosition: PointF = PointF(0.0f, 0.0f)
+    private val loadingRectangle = Rect()
+    private var loadingProgress = 0
+    private var textColor = 0
+    private var arcColor = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var valueAnimator = ValueAnimator()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    public var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        when (new) {
+            ButtonState.Loading -> {
+                valueAnimator = ValueAnimator.ofInt(0, 360).setDuration(2000)
+                    .apply {
+                        addUpdateListener {
+                            loadingProgress = it.animatedValue as Int
+                            invalidate()
+                        }
+                        repeatCount = ValueAnimator.INFINITE
+                        repeatMode = ValueAnimator.RESTART
 
+                        start()
+                    }
+            }
+            ButtonState.Completed -> {
+
+            }
+        }
     }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 60.0f
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
 
     init {
-
+        isClickable = true
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            textColor = getColor(R.styleable.LoadingButton_textColor, 0)
+            arcColor = getColor(R.styleable.LoadingButton_arcColor, 0)
+        }
     }
-
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        paint.color = resources.getColor(R.color.colorPrimary)
 
+        // get the canvas size
+        val canvasRect = Rect()
+        canvas?.getClipBounds(canvasRect)
+        val canvasHeight = canvasRect.height()
+        val canvasWidth = canvasRect.width()
+
+        // Draw the rectnagle and label that says "Download"
+        canvas?.drawRect(canvasRect, paint)
+        paint.color = textColor
+        val label = resources.getString(R.string.download_string)
+        pointPosition.computeXYForLabel()
+        canvas?.drawText(label, pointPosition.x, pointPosition.y + 15, paint)
+
+        if (buttonState == ButtonState.Loading) {
+            // Draw the underlying original rectangle
+            paint.color = resources.getColor(R.color.colorPrimary)
+            canvas?.drawRect(canvasRect, paint)
+
+            // Draw the darker animated rectangle using value animator over the original rect
+            paint.color = resources.getColor(R.color.colorPrimaryDark)
+            loadingRectangle.set(0,0, width * loadingProgress / 360, height)
+            canvas?.drawRect(loadingRectangle, paint)
+
+            // Draw the label "we are loading"
+            paint.color = textColor
+            val label = resources.getString(R.string.we_are_loading)
+            pointPosition.computeXYForLabel()
+            canvas?.drawText(label, pointPosition.x, pointPosition.y + 15, paint)
+
+            // Draw the arc
+            paint.color = arcColor
+            // Calculate the "we are loading" rect so we can place the arc right after it
+            val textRect = Rect()
+            paint.getTextBounds(label, 0, label.length, textRect)
+            val rectF = RectF()
+            val arcWidth = 30F
+            rectF.set(
+                (canvasWidth / 2f + textRect.width()) - arcWidth,
+                (canvasHeight/2) - arcWidth,
+                (canvasWidth / 2f + textRect.width()) + arcWidth,
+                (canvasHeight/2) + arcWidth
+            )
+
+            canvas?.drawArc(
+                rectF,
+                90F, (loadingProgress).toFloat(), true, paint
+            )
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -43,4 +124,8 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    private fun PointF.computeXYForLabel() {
+        x = (widthSize / 2 ).toFloat()
+        y = (heightSize / 2).toFloat()
+    }
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -8,7 +8,11 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -38,26 +42,52 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (id == downloadID) {
+                Log.d("DOWNLOAD", "Download is done!")
+                custom_button.buttonState = ButtonState.Completed
+            }
         }
     }
 
     private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
+
+        var url: String=""
+        val loadAppGroup = findViewById<RadioGroup>(R.id.radio_load_group)
+        when(loadAppGroup.checkedRadioButtonId) {
+            R.id.glide_button -> url = GLIDE_URL
+            R.id.udacity_button -> url = UDACITY_URL
+            R.id.retrofit_button -> url = RETROFIT_URL
+        }
+
+        if (url.isEmpty()) {
+            Toast.makeText(this,
+                getString(R.string.choose_message),
+                Toast.LENGTH_LONG)
+                .show()
+            return
+        } else {
+            Log.d("DOWNLOAD", url)
+            custom_button.buttonState = ButtonState.Loading
+            val request = DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadID =
+                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        }
     }
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val UDACITY_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
